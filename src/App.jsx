b7fb4b2876f5eaa4104e67c03bb7525069e290f8
chandan <chandanraaz91@gmail.com>
2024-05-28 17:57:54 +0530

    react with vite created

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>
+          Chandan Raj
+        </button>      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
